ff273c7db8a2e965981b7000b00b680196f31626
ImDaNoob <42329466+ImDaNoob@users.noreply.github.com>
2018-10-31 12:23:49 -0400

    Improved Comments and Variable Names

--- BucketSort.java
+++ BucketSort.java
@@ -40,27 +40,27 @@ public class BucketSort
         int max = list[0];
         int min = list[0];
         
-        for (int i = 1; i < list.length; i++) //Find the max and min of the list.
+        for (int potentialMaxOrMin = 1; potentialMaxOrMin < list.length; potentialMaxOrMin++) //Find the max and min of the list.
         {
-            if (list[i] > max) 
-                max = list[i];
-            else if (list[i] < min)
-                min = list[i];
+            if (list[potentialMaxOrMin] > max) 
+                max = list[potentialMaxOrMin];
+            else if (list[potentialMaxOrMin] < min)
+                min = list[potentialMaxOrMin];
         }
         
         int[] counterArray = new int[max - min + 1]; //Initialize a counter array to count each occurence of each element.
         
-        for (int i : list) //Iterate through the original array, adding one to the index of the element in counter.
+        for (int element : list) //Iterate through the original array, adding one to the index of the element in counter.
         {
-            counterArray[i - min]++;
+            counterArray[element - min]++; //The minimum element has an index of 0 relating to it in the counter array, requiring the subraction of the min value to reach the proper bucket.
         }
         
         int tracker = 0; //Keeps track of where we are placing the elements in the original array.
-        for (int i = 0; i < counterArray.length; i++) //Iterate through the counter array to place each element in their spot in the original array.
+        for (int bucket = 0; bucket < counterArray.length; bucket++) //Iterate through each bucket in the counter array to place the elements back in the original list in the proper order.
         {
-            for (int j = 1; j <= counterArray[i]; j++) //Makes sure dupicate elements are replaced the correct amount of times.
+            for (int element = 1; element <= counterArray[bucket]; element++) //Makes sure dupicate elements are replaced the correct amount of times.
             {
-                list[tracker] = i + min; //Places the element in its correct spot in the original array.
+                list[tracker] = bucket + min; //Places the element in its correct spot in the original array.
                 tracker++; 
             }
         }

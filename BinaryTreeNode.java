d92a3248086dff45da2edd76d2e02363c7ca3aec
ImDaNoob <42329466+ImDaNoob@users.noreply.github.com>
2018-12-04 12:54:15 -0500

    Add files via upload

--- BinaryTreeNode.java
+++ BinaryTreeNode.java
@@ -1,35 +1,33 @@
 //Benjamin Chappell
 
-import java.util.ArrayList<E>
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Queue;
 
 public class BinaryTreeNode<E>
 {
     private E data;
-    private BinaryTreeNode<E> leftChlld;
+    private BinaryTreeNode<E> leftChild;
     private BinaryTreeNode<E> rightChild;
 
     public BinaryTreeNode() 
     {
-        data = null;
-        leftChild = null;
-        rightChild = null;
+        this(null);
     }
     
     public BinaryTreeNode(E d)
     {
-        data = d;
-        leftChild = null;
-        rightChild = null;
+        this(d, null, null);
     }
     
     public BinaryTreeNode(E d, BinaryTreeNode<E> rChild, BinaryTreeNode<E> lChild)
     {
         data = d;
         rightChild = rChild;
-        liftChild = lChild;
+        leftChild = lChild;
     }
     
-    public E getData()
+     public E getData()
     {
         return data;
     }
@@ -66,35 +64,44 @@ public class BinaryTreeNode<E>
         return false;
     }
     
+    public boolean isEmpty()
+    {
+        if (data == null)
+            return true;
+        return false;
+    }
+    
     public int height()
     {
-        if (this.data == null or this.isLeaf())
+        if (this.data == null || this.isLeaf())
         {
             return 0;
         }
         else
         {
             int rightHeight = this.rightChild.height();
-            int leftChild = this.leftChild.height();
+            int leftHeight = this.leftChild.height();
             
             if (rightHeight > leftHeight)
-                return 1 + rightHeight
-            else
-                return 1 + leftHeight
+                return 1 + rightHeight;
+            return 1 + leftHeight;
         }
-    }   
+    }
     
-    
+    public String toString()
+    {
+        return data.toString();
+    }
     
     public ArrayList<E> preorder()
     {
-        ArrayList<E> preordered = new ArrayList<E>;
+        ArrayList<E> preordered = new ArrayList<E>();
         
         if (this != null)
         {
             preordered.add(data);
-            preordered.addAll(rightChild.preorder());
             preordered.addAll(leftChild.preorder());
+            preordered.addAll(rightChild.preorder());
         }
         
         return preordered;
@@ -102,6 +109,48 @@ public class BinaryTreeNode<E>
     
     public ArrayList<E> postorder()
     {
+        ArrayList<E> postordered = new ArrayList<E>();
         
+        if (this != null)
+        {
+            postordered.addAll(leftChild.postorder());
+            postordered.addAll(rightChild.postorder());
+            postordered.add(data);
+        }    
+        return postordered;
+    }
+    
+    public ArrayList<E> inorder()
+    {
+        ArrayList<E> inordered = new ArrayList<E>();
+        
+        if (this != null)
+        {
+            inordered.addAll(leftChild.inorder());
+            inordered.add(data);
+            inordered.addAll(rightChild.inorder());
+        }
+        return inordered;
+    }
+    
+    public ArrayList<E> levelOrder()
+    {
+        ArrayList<E> levelOrdered = new ArrayList<E>();
+        Queue<BinaryTreeNode<E>> levelQueue = new LinkedList<BinaryTreeNode<E>>();
+        
+        levelQueue.add(this);
+        
+        while (levelQueue.peek() != null)
+        {
+            BinaryTreeNode<E> currentNode = levelQueue.poll();
+            levelOrdered.add(currentNode.data);
+            
+            if (currentNode.leftChild != null)
+                levelQueue.add(currentNode.leftChild);
+                
+            if (currentNode.rightChild != null)
+                levelQueue.add(currentNode.rightChild);
+        }
+        return levelOrdered;
     }
 }
---
+++ BinaryTreeTest.java
@@ -0,0 +1,14 @@
+//Benjamin Chappell
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class BinaryTreeTest
+{
+    public static void main(String args[])
+    {
+        System.out.println("You've been gnomed");
+        Queue<Integer> test1 = new LinkedList<Integer>;
+        
+        int[] test1Array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+}

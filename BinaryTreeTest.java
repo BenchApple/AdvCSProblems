559dd4166d3aaf6cd63484d19b297e6cf7d54d79
ImDaNoob <42329466+ImDaNoob@users.noreply.github.com>
2018-12-05 12:54:45 -0500

    Add files via upload

--- BinaryTreeTest.java
+++ BinaryTreeTest.java
@@ -8,7 +8,24 @@ public class BinaryTreeTest
     public static void main(String args[])
     {
         System.out.println("You've been gnomed");
-        Queue<Integer> test1 = new LinkedList<Integer>;
+        Object[] test = new Object[10];
         
-        int[] test1Array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < 10; i++)
+        {
+            (BinaryTreeNode<Integer>)(test[i]) = new BinaryTreeNode<Integer>(i);
+        }
+       
+        /*
+        test[0].setRightChild(test[1]);     test[0].setLeftChild(test[2]);
+        test[1].setRightChild(test[3]);     test[1].setLeftChild(test[4]);
+        test[2].setRightChild(null);        test[2].setLeftChild(test[5]);
+        test[3].setRightChild(null);        test[3].setLeftChild(null);
+        test[4].setRightChild(test[6]);     test[4].setLeftChild(test[7]);
+        test[5].setRightChild(test[8]);     test[5].setLeftChild(test[9]);
+        test[6].setRightChild(null);        test[6].setLeftChild(null);
+        test[7].setRightChild(null);        test[7].setLeftChild(null);
+        test[8].setRightChild(null);        test[8].setLeftChild(null);
+        test[9].setRightChild(null);        test[9].setLeftChild(null);
+        */
+    }
 }

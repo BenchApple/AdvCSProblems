b23013145ef6f4f04cc662a6e559191d6a602d69
ImDaNoob <42329466+ImDaNoob@users.noreply.github.com>
2018-12-06 12:55:47 -0500

    Update BinaryTreeTest.java

--- BinaryTreeTest.java
+++ BinaryTreeTest.java
@@ -2,30 +2,37 @@
 
 import java.util.Queue;
 import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class BinaryTreeTest
 {
     public static void main(String args[])
     {
         System.out.println("You've been gnomed");
-        Object[] test = new Object[10];
+        ArrayList<BinaryTreeNode<Integer>> test = new ArrayList<BinaryTreeNode<Integer>>();
         
         for (int i = 0; i < 10; i++)
         {
-            (BinaryTreeNode<Integer>)(test[i]) = new BinaryTreeNode<Integer>(i);
+            test.add(new BinaryTreeNode<Integer>(i));
         }
        
-        /*
-        test[0].setRightChild(test[1]);     test[0].setLeftChild(test[2]);
-        test[1].setRightChild(test[3]);     test[1].setLeftChild(test[4]);
-        test[2].setRightChild(null);        test[2].setLeftChild(test[5]);
-        test[3].setRightChild(null);        test[3].setLeftChild(null);
-        test[4].setRightChild(test[6]);     test[4].setLeftChild(test[7]);
-        test[5].setRightChild(test[8]);     test[5].setLeftChild(test[9]);
-        test[6].setRightChild(null);        test[6].setLeftChild(null);
-        test[7].setRightChild(null);        test[7].setLeftChild(null);
-        test[8].setRightChild(null);        test[8].setLeftChild(null);
-        test[9].setRightChild(null);        test[9].setLeftChild(null);
-        */
+        
+        test.get(0).setLeftChild(test.get(1));     test.get(0).setRightChild(test.get(2));
+        test.get(1).setLeftChild(test.get(3));     test.get(1).setRightChild(test.get(4));
+        test.get(2).setLeftChild(null);            test.get(2).setRightChild(test.get(5));
+        test.get(3).setLeftChild(null);            test.get(3).setRightChild(null);
+        test.get(4).setLeftChild(test.get(6));     test.get(4).setRightChild(test.get(7));
+        test.get(5).setLeftChild(null);            test.get(5).setRightChild(test.get(9));
+        //test.get(6).setLeftChild(null);            test.get(6).setRightChild(null);
+        //test.get(7).setLeftChild(null);            test.get(7).setRightChild(null);
+        //test.get(8).setLeftChild(null);            test.get(8).setRightChild(null);
+        test.get(9).setLeftChild(test.get(8));            test.get(9).setRightChild(null);
+        
+        
+        System.out.println("Preorder " + test.get(0).preorder());
+        System.out.println("Inorder " + test.get(0).inorder());
+        System.out.println("Postorder " + test.get(0).postorder());
+        System.out.println("Level Order " + test.get(0).levelOrder());
+        System.out.println("Height of root " + test.get(0).height());
     }
 }

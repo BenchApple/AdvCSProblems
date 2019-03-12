169118e2277e25029f5c96d6ddc66681b8a1eb4a
ImDaNoob <bjchappell02@icloud.com>
2019-03-12 12:51:37 -0400

    Finished Build tree, unless there are bugs

---
+++ Huffman/HuffmanDecoder.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+
+import com.sun.source.tree.BinaryTree;
+
+// Benjamin Chappell
+
+public class HuffmanDecoder
+{
+    public static void main(String[] args) throws java.io.IOException
+    {
+        BinaryTreeNode root = new BinaryTreeNode<>();
+    }
+
+    private BinaryTreeNode<> buildTree() throws IOException
+    {
+        BinaryTreeNode current = new BinaryTreeNode<>();
+
+        if (System.in.read() == 0)
+        {
+            current.setLeftChild(buildTree());
+            current.setRightChild(buildTree());
+        }
+        else if (System.in.read() == 1)
+        {
+            current.setData((char)System.in.read());
+        }
+    }
+}

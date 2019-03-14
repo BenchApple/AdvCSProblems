0aae46a7c6d5bd296aa2327ce3286566c13d2386
ImDaNoob <bjchappell02@icloud.com>
2019-03-14 12:44:09 -0400

    finished the main method

--- Huffman/HuffmanDecoder.java
+++ Huffman/HuffmanDecoder.java
@@ -7,55 +7,50 @@ public class HuffmanDecoder
     public static void main(String[] args) throws java.io.IOException
     {
         BinaryTreeNode tree = buildTree();
-        ArrayList<BinaryTreeNode<Character>> traversal = tree.levelOrderTraversal();
-        
-        for (BinaryTreeNode i : traversal)
-        {
-            System.out.println(i.getData());
-        }
 
-        //decode(tree);
+        decode(tree);
     }
 
-    private static BinaryTreeNode buildTree() throws java.io.IOException
+    private static BinaryTreeNode<Character> buildTree() throws java.io.IOException
     {
         BinaryTreeNode current = new BinaryTreeNode<Character>();
 
-        if ((char)System.in.read() == '0')
+	int next = System.in.read();
+        if ((char)next == '0')
         {
             current.setLeftChild(buildTree());
             current.setRightChild(buildTree());
         }
-        else if ((char)System.in.read() == '1')
+        else if ((char)next == '1')
         {
-            current.setData((char)System.in.read());
+            current.setData((char)(System.in.read()));
         }
 
         return current;
     }
 
-    private static void decode(BinaryTreeNode root) throws java.io.IOException
+    private static void decode(BinaryTreeNode<Character> root) throws java.io.IOException
     {
         int next = System.in.read();
-        BinaryTreeNode currentNode = root;
+        BinaryTreeNode<Character> currentNode = root;
 
         while (next != -1)
         {
-            if (currentNode.getLeftChild() == null)
-            {
-                System.out.println(currentNode.getData());
-                currentNode = root;
-            }
-            else if ((char)next == '0')
+            if ((char)next == '0')
             {
                 currentNode = currentNode.getLeftChild();
-                next = System.in.read();
             }
             else if ((char)next == '1')
             {
                 currentNode = currentNode.getRightChild();
-                next = System.in.read();
             }
+
+	    if (currentNode.getLeftChild() == null)
+	    {
+		System.out.print(currentNode.getData());
+		currentNode = root;
+	    }
+	    next = System.in.read();
         }
     }
 }

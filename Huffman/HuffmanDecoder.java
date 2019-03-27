ae3a3eaa1d4d570b57fd56509b2645fac831deb3
ImDaNoob <bjchappell02@icloud.com>
2019-03-27 12:51:25 -0400

    added files for graph

--- Huffman/HuffmanDecoder.java
+++ Huffman/HuffmanDecoder.java
@@ -11,16 +11,20 @@ public class HuffmanDecoder
         decode(tree);
     }
 
+    //Build the tree from the read in data.
     private static BinaryTreeNode<Character> buildTree() throws java.io.IOException
     {
         BinaryTreeNode current = new BinaryTreeNode<Character>();
 
-	int next = System.in.read();
+        int next = System.in.read();
+        //0 indicates a new node to be made inorder.
         if ((char)next == '0')
         {
+            //Recursively build the right and half sides of the trees inorder.
             current.setLeftChild(buildTree());
             current.setRightChild(buildTree());
         }
+        // 1 indicates a new leaf to be made with the data being the character following the 1.
         else if ((char)next == '1')
         {
             current.setData((char)(System.in.read()));
@@ -29,27 +33,33 @@ public class HuffmanDecoder
         return current;
     }
 
+    // Decode the message using the created tree and the read in data.
     private static void decode(BinaryTreeNode<Character> root) throws java.io.IOException
     {
+        //Read in the first character as an integer, and store the current node as the root of the tree.
         int next = System.in.read();
         BinaryTreeNode<Character> currentNode = root;
 
+        // Loop until there is nothing left to read, where the system will read an integer negative 1.
         while (next != -1)
         {
+            // Move to the left in the tree when the next read is a 0.
             if ((char)next == '0')
             {
                 currentNode = currentNode.getLeftChild();
             }
+            // Move to the right in the tree when the next read is 1.
             else if ((char)next == '1')
             {
                 currentNode = currentNode.getRightChild();
             }
 
-	    if (currentNode.getLeftChild() == null)
-	    {
-		System.out.print(currentNode.getData());
-		currentNode = root;
-	    }
+            // If the current node is a leaf, print the data in the node and reset the current node to be the root.
+	        if (currentNode.getLeftChild() == null)
+	        {
+		        System.out.print(currentNode.getData());
+		        currentNode = root;
+	        }
 	    next = System.in.read();
         }
     }
--- hpq/NullElementException.java
+++ hpq/NullElementException.java
@@ -4,12 +4,11 @@ public class NullElementException extends RuntimeException
 {
     public NullElementException(String msg)
     {
-        System.out.println(msg);
+        super("The heap cannot store null elements. Do not attempt to add Null Elements to the heap.");
     }
 
     public NullElementException()
     {
-        String msg = "The heap cannot store null elements. Do not attempt to add Null Elements to the heap.";
-        System.out.println(msg);
+        super();
     }
 }

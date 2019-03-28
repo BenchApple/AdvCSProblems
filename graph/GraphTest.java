f89b97bb8551a74d9fe4a64df8128b3b3123aa08
ImDaNoob <bjchappell02@icloud.com>
2019-03-28 14:12:55 -0400

    moved into testing

---
+++ graph/GraphTest.java
@@ -0,0 +1,18 @@
+// Benjamin Chappell
+
+import java.util.*;
+
+public class GraphTest
+{
+    public static void main(String args[])
+    {
+        Graph g = new Graph();
+
+        ArrayList<Integer> graph = new ArrayList<Integer>();
+
+        graph = g.DFS();
+
+        for (int i : graph)
+            System.out.println(i);
+    }
+}
